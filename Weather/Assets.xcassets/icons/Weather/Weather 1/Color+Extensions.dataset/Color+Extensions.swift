2fdd96c45841e3c790a9a680c41ea849503d9a40
Ryan Rouxinol <ryanrouxinol17@gmail.com>
2024-07-12 20:50:58 -0300

    first commit

---
+++ Weather/Assets.xcassets/icons/Weather/Weather 1/Color+Extensions.dataset/Color+Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  Color+Extensions.swift
+//  Weather
+//
+//  Created by Ryan Rouxinol on 01/01/24.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static let contrastColor = UIColor(named: "whiteAssetColor")
+    static let primaryColor = UIColor(named: "blueAssetColor")
+    static let softGray = UIColor(named: "softGrey")
+}
+
